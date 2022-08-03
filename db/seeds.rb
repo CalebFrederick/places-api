32b02ca814cde0d944a417c99702feb49a88bc3f
CalebFrederick <caleb@heckfamily.com>
2022-08-03 17:42:38 -0400

    places-api

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# Place.create(name: "Evansville", address: "8810 Whispering Tree Lane")
+# Place.create(name: "Central", address: "5400 N First Ave")
+# Place.create(name: "Home", address: "255 S 4th St")
